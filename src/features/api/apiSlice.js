4410fc4f7dc67cb930fb9720feca945223932b55
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-03-28 14:45:57 +0600

    added

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,9 +4,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query"
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:9000/"
+        baseUrl: "http://localhost:9000"
     }),
     endpoints: (builder) => ({
+        getVideos: builder.query({
 
+        })
     })
 })
